899d8e63036ed347fa60acbab5a96af52b533316
stufro <stuart.frosty@gmail.com>
2022-05-24 21:01:43 +0100

    test: add additional test for chord sheet modeller

--- spec/models/chord_sheet_modeller_spec.rb
+++ spec/models/chord_sheet_modeller_spec.rb
@@ -8,11 +8,29 @@ describe ChordSheetModeller do
 Some lyrics here
       )
     }
+
     it "returns the chord sheet string modelled as JSON" do
       expect(described_class.new(raw_content).build).to eq([
         { type: :chords, content: " G     Em    C"},
         { type: :lyrics, content: "Some lyrics here"},
       ])
+    end
+
+    context "when there are empty lines" do
+      let(:raw_content) {
+        %Q(
+
+ G     Em    C
+Some lyrics here
+        )
+      }
+
+      it "removes them" do
+        expect(described_class.new(raw_content).build).to eq([
+          { type: :chords, content: " G     Em    C"},
+          { type: :lyrics, content: "Some lyrics here"},
+        ])
+      end
     end
   end
 end
