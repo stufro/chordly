ff20c0575c39a3ac70e116d316d8407febc0f270
stufro <stuart.frosty@gmail.com>
2022-05-28 15:01:49 +0100

    test: refactor transposing specs

--- spec/models/chord_sheet_spec.rb
+++ spec/models/chord_sheet_spec.rb
@@ -12,27 +12,30 @@ describe ChordSheet do
     let(:direction) { "up" }
 
     context "when the given direction is up" do
-      let(:expected_content) {[
-        { "type" => "lyrics", "content" => "Some song lyrics"},
-        { "type" => "chords", "content" => "G♭5 C♭5 E♭m5"}
-      ]}
+      let(:expected_chords) { "G♭5 C♭5 E♭m5" }
 
       it "increases all the chords by a semitone" do
         subject.transpose(:up)
-        expect(subject.content).to eq(expected_content)
+        expect(subject.content[1]["content"]).to eq(expected_chords)
+      end
+    end
+
+    context "when the given direction is down" do
+      let(:expected_chords) { "E5 A5 C#m5" }
+
+      it "decreases all the chords by a semitone" do
+        subject.transpose(:down)
+        expect(subject.content[1]["content"]).to eq(expected_chords)
       end
     end
 
     context "when there is whitespace between chords" do
       let(:original_chords) { " F  Bb   Dm" }
-      let(:expected_content) {[
-        { "type" => "lyrics", "content" => "Some song lyrics"},
-        { "type" => "chords", "content" => " G♭5  C♭5   E♭m5"}
-      ]}
+      let(:expected_chords) { " G♭5  C♭5   E♭m5" }
 
       it "maintains the whitespace" do
         subject.transpose(:up)
-        expect(subject.content).to eq(expected_content)
+        expect(subject.content[1]["content"]).to eq(expected_chords)
       end
     end 
   end
