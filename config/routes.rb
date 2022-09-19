c1ef1dc50d088bb6960c49bd0d42fae1541c3bf9
stufro <stuart.frosty@gmail.com>
2022-09-19 21:33:02 +0100

    feat: add home page with boilerplate content

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :helps
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "home#index"
 
-  root "chord_sheets#index"
   resources :chord_sheets, only: %i[index new create show update] do
     put :transpose
   end
+
+  resource :home, only: %i[index]
 end
