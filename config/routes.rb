0de2c10e281b1a9088cba65a770f1c6304c69440
stufro <stuart.frosty@gmail.com>
2022-10-03 14:42:03 +0100

    feat: steel thread for sign up + login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "home#index"
+
+  devise_scope :user do
+    authenticated :user do
+      root "home#index", as: :authenticated_root
+
+    end
+
+    unauthenticated do
+      root "devise/sessions#new", as: :unauthenticated_root
+    end
+  end
 
   resources :chord_sheets, only: %i[index new create show update] do
     put :transpose
