e027bf18a94a9b248047de1739a60223ffed08e7
stufro <stuart.frosty@gmail.com>
2022-10-04 13:23:05 +0100

    feat: resetting a forgotten password

--- spec/cypress/integration/login.spec.js
+++ spec/cypress/integration/login.spec.js
@@ -1,5 +1,3 @@
-import * as helper from "../support/helpers"
-
 describe("Signing up for chordly", () => {
   afterEach(() => {
     cy.app("clean")
@@ -20,14 +18,14 @@ describe("Signing up for chordly", () => {
 })
 
 describe("Logging in with existing account", () => {
-  afterEach(() => {
-    cy.app("clean")
-  })
-
   beforeEach(() => {
     cy.appFactories([
       ["create", "user", { email: "a@a.com", password: "123456789" } ]
     ])
+  })
+
+  afterEach(() => {
+    cy.app("clean")
   })
 
   it("logs the user in", () => {
@@ -39,5 +37,50 @@ describe("Logging in with existing account", () => {
     cy.contains("Logout")
     cy.contains("Signed in successfully.")
     cy.contains("My Chord Sheets")
+  })
+})
+
+describe("Logging out", () => {
+  beforeEach(() => {
+    cy.appFactories([
+      ["create", "user", { email: "a@a.com", password: "123456789" } ]
+    ])
+  })
+
+  afterEach(() => {
+    cy.app("clean")
+  })
+
+  it("logs the user out", () => {
+    cy.visit("/")
+    cy.get("#user_email").type("a@a.com")
+    cy.get("#user_password").type("123456789")
+    cy.get("#login-button").click()
+    cy.contains("Logout").click()
+
+    cy.contains("Log in")
+    cy.contains("Forgot your password?")
+  })
+})
+
+describe("Resetting a forgotten password", () => {
+  beforeEach(() => {
+    cy.appFactories([
+      ["create", "user", { email: "a@a.com", password: "123456789" } ]
+    ])
+  })
+
+  afterEach(() => {
+    cy.app("clean")
+  })
+
+  it("sends the password reset instructions", () => {
+    cy.visit("/")
+    cy.contains("Forgot your password?").click()
+    cy.get("#user_email").type("a@a.com")
+    cy.contains("Send me reset password instructions").click()
+
+    cy.contains("Log in")
+    cy.contains("You will receive an email with instructions on how to reset your password in a few minutes.")
   })
 })
