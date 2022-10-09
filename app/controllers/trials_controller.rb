ab9b0456ec4b01427b610f52e215a48e27fda59f
stufro <stuart.frosty@gmail.com>
2022-10-09 16:22:32 +0100

    feat: only keep one trial sheet per user session

--- app/controllers/trials_controller.rb
+++ app/controllers/trials_controller.rb
@@ -2,7 +2,9 @@ class TrialsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def new
-    session[:trial_user_id] = SecureRandom.base64
+    session[:trial_user_id] ||= SecureRandom.base64
+    ChordSheet.where(trial_user_id: session[:trial_user_id]).destroy_all
+
     @chord_sheet = ChordSheet.new(
       name: "My Song", trial: true, trial_user_id: session[:trial_user_id],
       content: "G   Em   D\r\nType or paste your song here"
