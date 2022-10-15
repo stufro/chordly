66035544790a93c7bb949aa576914b3e6555ec52
stufro <stuart.frosty@gmail.com>
2022-10-15 10:50:29 +0100

    feat: configure AWS SES for sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,15 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+
+  config.x.mail_from = %(Chordly <support@chordly.co.uk>)
   config.action_mailer.default_url_options = { host: "https://chordly.co.uk", port: 443 }
+  config.action_mailer.smtp_settings = {
+    address: "email-smtp.eu-west-2.amazonaws.com", 
+    user_name: Rails.application.credentials.dig(:aws, :smtp_username),
+    password: Rails.application.credentials.dig(:aws, :smtp_password) 
+  }
+  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
