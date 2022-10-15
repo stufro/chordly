8851153de46216bb84c91d08ef09bfa9d628c78c
stufro <stuart.frosty@gmail.com>
2022-10-15 22:56:33 +0100

    refactor: compress css & js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,8 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
