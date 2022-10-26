29b46f969577e14436c65a55a196510ee365e835
stufro <stuart.frosty@gmail.com>
2022-10-26 16:26:20 +0100

    feat: lock down admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,16 @@
 class AdminController < ApplicationController
+  before_action :authorize_admin
+
   def index
     @chord_sheets = ChordSheet.all
     @users = User.all
+  end
+
+  private
+
+  def authorize_admin
+    return if current_user.admin?
+
+    redirect_to "/", alert: "You are not authorized to view this page", code: 401
   end
 end
