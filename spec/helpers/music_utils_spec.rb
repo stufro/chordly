2cdd3ff2dd561f4ea25b659153959648ecbddd85
stufro <stuart.frosty@gmail.com>
2022-10-30 15:24:18 +0000

    test: don't require the whole of rails for MusicUtils tests

--- spec/helpers/music_utils_spec.rb
+++ spec/helpers/music_utils_spec.rb
@@ -1,44 +1,46 @@
-require "rails_helper"
+require_relative "../../app/helpers/music_utils"
 
 describe MusicUtils do
+  let(:subject) { Class.new { extend MusicUtils } }
+
   describe "#extract_note" do
     it "returns the note from the given chords" do
       chord = "C#m7"
-      expect(extract_note(chord)).to eq "C#"
+      expect(subject.extract_note(chord)).to eq "C#"
     end
 
     context "when there is not a valid note in the string" do
       it "returns the given input" do
         not_a_chord = "Another string"
-        expect(extract_note(not_a_chord)).to eq not_a_chord
+        expect(subject.extract_note(not_a_chord)).to eq not_a_chord
       end
     end
 
     context "with some different chord types" do
       it "returns the root note" do
         %w[Bbmaj Bb5 Bb7 Bbm5 Bbm7 Bbmin7 Bbmin Bbsus4 Bbsus2 Bb9 Bbm9 Bb13 Bbm13].each do |chord|
-          expect(extract_note(chord)).to eq "Bb"
+          expect(subject.extract_note(chord)).to eq "Bb"
         end
       end
     end
 
     context "with an inversion" do
       it "returns the root note" do
-        expect(extract_note("G#/D#")).to eq "G#"
+        expect(subject.extract_note("G#/D#")).to eq "G#"
       end
     end
   end
 
-  describe "#extract_note!" do
+  describe "#subject.extract_note!" do
     it "returns the note from the given chords" do
       chord = "Am"
-      expect(extract_note!(chord)).to eq "A"
+      expect(subject.extract_note!(chord)).to eq "A"
     end
 
     context "when an invalid chord is given" do
       it "raises an ArgumentError exception" do
         not_a_chord = "Another string"
-        expect { extract_note!(not_a_chord) }.to raise_exception(ArgumentError)
+        expect { subject.extract_note!(not_a_chord) }.to raise_exception(ArgumentError)
       end
     end
   end
