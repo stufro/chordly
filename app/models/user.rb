342dfa4eb3190e40e935acc2c8078d17133eff96
stufro <stuart.frosty@gmail.com>
2022-11-26 17:15:00 +0000

    feat: set lists feature flag per user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include Flipper::Identifier
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
