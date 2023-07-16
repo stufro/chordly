2b4f6fba941f8a15ab153791987146e25f135628
stufro <stuart.frosty@gmail.com>
2023-07-16 20:26:33 +0100

    build: update gems

--- config/initializers/cypress_on_rails.rb
+++ config/initializers/cypress_on_rails.rb
@@ -1,6 +1,6 @@
 if defined?(CypressOnRails)
   CypressOnRails.configure do |c|
-    c.cypress_folder = File.expand_path("#{__dir__}/../../spec/cypress")
+    c.install_folder = File.expand_path("#{__dir__}/../../spec/cypress")
     # WARNING!! CypressOnRails can execute arbitrary ruby code
     # please use with extra caution if enabling on hosted servers or starting your local server on 0.0.0.0
     c.use_middleware = !Rails.env.production?
