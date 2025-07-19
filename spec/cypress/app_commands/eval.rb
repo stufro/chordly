c8ea70429c196529d47cc8a0771a23b2c4b63c22
stufro <stuart.frosty@gmail.com>
2025-07-19 13:29:36 +0100

    chore: Update gems

--- spec/cypress/app_commands/eval.rb
+++ spec/cypress/app_commands/eval.rb
@@ -1,1 +1,6 @@
-Kernel.eval(command_options) unless command_options.nil?
+Kernel.eval(command_options) unless command_options.nil? # rubocop:disable Security/Eval
+
+# This file is used to evaluate Ruby code dynamically. It is used to communicate between Cypress tests
+# and the Ruby backend for test setup etc.
+# It should be used with caution as it can execute arbitrary code.
+# Ensure that `command_options` is sanitized and safe before using this.
