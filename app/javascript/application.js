73d67c4570616a34e545aed0f40d6a5fe5172b7a
stufro <stuart.frosty@gmail.com>
2026-03-08 21:05:14 +0000

    fix: Redirect to maintenance when enabled from turbo requests

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,7 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 
+
 import "trix"
 import "@rails/actiontext"
 import LocalTime from "local-time"
@@ -18,6 +19,14 @@ function loadAdsense() {
     }
   })
 }
+
+// Redirect to maintenance page on 503 responses
+document.addEventListener("turbo:frame-missing", (event) => {
+  if (event.detail.response.status === 503) {
+    event.preventDefault()
+    window.location.href = event.detail.response.url
+  }
+})
 
 document.addEventListener("DOMContentLoaded", loadAdsense)
 document.addEventListener("turbo:render", loadAdsense)
